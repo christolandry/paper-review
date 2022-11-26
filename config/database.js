d6d31bf526ac11aa8803615454a74859e3ac7a40
Christo Landry <Christo.Landry@gmail.com>
2022-11-26 11:48:26 -0500

    Update Connection string for MongoDB for Vercel integration

--- config/database.js
+++ config/database.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.DB_STRING, {
+    const connectionString = process.env.MONGODB_URI ? process.env.MONGODB_URI : process.env.DB_STRING
+    const conn = await mongoose.connect(connectionString, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false,
