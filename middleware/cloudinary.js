3ee7d04c0a91cdb02e90d7533d087a33a8391346
Christo Landry <Christo.Landry@gmail.com>
2022-11-30 12:02:20 -0500

    Add: Hardcoded cloud name

--- middleware/cloudinary.js
+++ middleware/cloudinary.js
@@ -3,7 +3,8 @@ const cloudinary = require("cloudinary").v2;
 require("dotenv").config({ path: "./config/.env" });
 
 cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
+  cloud_name: 'dy1dtpv72',
+  // cloud_name: process.env.CLOUD_NAME,
   api_key: '341219585238167',
   // api_key: process.env.API_KEY,
   api_secret: 'NA8dfaD_tqeFlfJGIXwDpdMK98U',
