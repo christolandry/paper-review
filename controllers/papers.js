2fb7f17f7c4d39d8281fff462ae9011eb47b4ffd
Christo Landry <Christo.Landry@gmail.com>
2022-12-01 14:47:25 -0500

    Add: More testing on environmental variable

--- controllers/papers.js
+++ controllers/papers.js
@@ -64,6 +64,8 @@ module.exports = {
       // Upload image to cloudinary
       console.log("***************** Check 0 ****************")
       console.log(`Environmental Variable Key Check: ${process.env.API_KEY}`)
+      console.log(`Do they match: ${process.env.API_KEY === '341219585238167' ? "Yes" : "No"}`)
+      console.log(`Do they match: ${process.env.API_KEY == '341219585238167' ? "Yes" : "No"}`)
       const result = await cloudinary.uploader.upload(req.file.path);
       console.log("***************** Check 0.5 ****************")
       await PaperCounter.findOneAndUpdate(
